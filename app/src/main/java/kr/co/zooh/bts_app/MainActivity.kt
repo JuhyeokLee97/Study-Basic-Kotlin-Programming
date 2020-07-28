d25cf9c76667480db8d4b45a9e1b0e6f58898203
ZooH <zero5.two4@gmail.com>
2020-07-29 01:15:13 +0900

    BTS-APP v.1.1

--- app/src/main/java/kr/co/zooh/bts_app/MainActivity.kt
+++ app/src/main/java/kr/co/zooh/bts_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.zooh.bts_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -12,7 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_one.setOnClickListener {
-            Toast.makeText(this, "Toast Message", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "정국 사진", Toast.LENGTH_LONG).show()
+
+            var intent = Intent(this, Photo1_Activity::class.java)
+            startActivity(intent)
         }
+
     }
 }
---
+++ app/src/main/java/kr/co/zooh/bts_app/Photo1_Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.zooh.bts_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Photo1_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo1_)
+    }
+}
