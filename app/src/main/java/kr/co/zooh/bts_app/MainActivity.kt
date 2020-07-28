36d1ee9565cf0f1f0353121944282fb2dff3c609
ZooH <zero5.two4@gmail.com>
2020-07-29 00:47:20 +0900

    BTS-APP v.1

--- app/src/main/java/kr/co/zooh/bts_app/MainActivity.kt
+++ app/src/main/java/kr/co/zooh/bts_app/MainActivity.kt
@@ -2,11 +2,17 @@ package kr.co.zooh.bts_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_one.setOnClickListener {
+            Toast.makeText(this, "Toast Message", Toast.LENGTH_LONG).show()
+        }
     }
 }
