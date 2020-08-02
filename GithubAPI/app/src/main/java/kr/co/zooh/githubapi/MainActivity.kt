21cbc4ed1f6d7219b7e2e66384136bef78fa4e73
ZooH <zero5.two4@gmail.com>
2020-08-03 02:45:15 +0900

    Github parse API v.2.1
    Adding View of List
    the items were from Github API of login-ID

--- GithubAPI/app/src/main/java/kr/co/zooh/githubapi/MainActivity.kt
+++ GithubAPI/app/src/main/java/kr/co/zooh/githubapi/MainActivity.kt
@@ -1,8 +1,16 @@
 package kr.co.zooh.githubapi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.listview_item.*
+import kotlinx.android.synthetic.main.listview_item.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,6 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     val BASE_URL = "https://api.github.com/search/"
+    val arr = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +42,32 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<UserData>, response: Response<UserData>) {
                 for (str in response?.body()?.items!!){
                     Log.e("MAINACTIVITY", str.login)
+                    arr.add(str.login)
                 }
+                listView.adapter = MainListviewAdapter(this@MainActivity, arr)
             }
 
         })
+    }
+    inner class MainListviewAdapter(val context : Context, val list : ArrayList<String>) : BaseAdapter(){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var view : View = LayoutInflater.from(context).inflate(R.layout.listview_item, null)
+            view.listview_item_id.setText(list.get(position))
+
+            return view
+        }
+
+        override fun getItem(position: Int): Any {
+            return 0
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return list.size
+        }
+
     }
 }
